46928756e1ee6bac2065f2c075b086946a70abeb
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-04 16:39:48 -0400

    test: createFetchDistritos.ts

---
+++ src/providers/DistritosProvider/fetchDistritos/createFetchDistritos.test.ts
@@ -0,0 +1,41 @@
+import { assertEquals, assertObjectMatch } from "@src/deps.ts";
+import { Result } from "@src/kinds/Result.ts";
+import { Distritos } from "../../../models/Distrito.ts";
+import { createFetchDistritos } from "./createFetchDistritos.ts";
+
+Deno.test("createFetchDistritos should fetch distritos", async () => {
+  const distritosJsonBody = [{
+    "distrito-id": 110020505,
+    "distrito-nome": "Porto Velho",
+    "municipio-id": "1100205",
+    "municipio-nome": "Porto Velho",
+    "UF-id": 11,
+    "UF-sigla": "RO",
+  }];
+
+  const distritos: Distritos = [{
+    "distrito-id": "110020505",
+    "distrito-nome": "Porto Velho",
+    "municipio-id": "1100205",
+    "municipio-nome": "Porto Velho",
+    "UF-id": "11",
+    "UF-sigla": "RO",
+  }];
+
+  const fetchDistritos = createFetchDistritos({
+    async fetch(input: string): Promise<Response> {
+      await Promise.resolve();
+
+      if (
+        input !==
+          "https://servicodados.ibge.gov.br/api/v1/localidades/distritos?view=nivelado"
+      ) {
+        throw new Error("It is not fetching from localidades api");
+      }
+
+      return Response.json(distritosJsonBody);
+    },
+  });
+  const distritosResult = await fetchDistritos();
+  assertEquals(distritosResult, Result.done(distritos));
+});
