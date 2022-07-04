43dbf40dff9e5bcae7c08189eb17e7d63fd9d27d
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-04 19:32:50 -0400

    test(createJsonRepository): createInsertItem

--- src/repositories/JsonRepository/createJsonRepository.test.ts
+++ src/repositories/JsonRepository/createJsonRepository.test.ts
@@ -1,13 +1,17 @@
 import { assertEquals } from "@src/deps.ts";
 import { Result } from "../../kinds/Result.ts";
+import { SafeWriteJson } from "../../shared/safeWriteJson/ISafeWriteJson.ts";
 
-import { createQueryAllItems } from "./createJsonRepository.ts";
+import {
+  createInsertItem,
+  createQueryAllItems,
+} from "./createJsonRepository.ts";
 
 Deno.test("FakeJsonRepository should query All", async () => {
   const queryAllItems = createQueryAllItems({
-    path: "teste.json",
+    path: "file.json",
     async safeReadJson(path) {
-      assertEquals(path, "teste.json");
+      assertEquals(path, "file.json");
 
       return await Result.done(["Item"]);
     },
@@ -17,3 +21,40 @@ Deno.test("FakeJsonRepository should query All", async () => {
 
   assertEquals(items, Result.done(["Item"]));
 });
+
+Deno.test("FakeJsonRepository should insert item", async () => {
+  let database: string[] = [];
+
+  const insertItem = createInsertItem({
+    path: "file.json",
+    async safeReadJson(path) {
+      await Promise.resolve();
+      assertEquals(path, "file.json");
+      return Result.done(database);
+    },
+    async safeWriteJson(path, data) {
+      await Promise.resolve();
+      assertEquals(path, "file.json");
+      database = data;
+      return Result.done(undefined);
+    },
+  });
+
+  const result = await insertItem("Item");
+
+  assertEquals(result, Result.done("Item"));
+
+  assertEquals(database, ["Item"]);
+});
+
+const createFakeSafeWriteJson = ({
+  database,
+}: {
+  database: any[];
+}): SafeWriteJson =>
+  async (path, data) => {
+    await Promise.resolve();
+    assertEquals(path, "file.json");
+    database = data;
+    return Result.done(undefined);
+  };
