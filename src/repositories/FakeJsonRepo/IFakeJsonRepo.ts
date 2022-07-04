b0bc67534c52672a81cbc4a92ef5bb1cf66084a7
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-04 15:51:16 -0400

    refactor: deleted FakeJsonRepo

--- src/repositories/FakeJsonRepo/IFakeJsonRepo.ts
+++
@@ -1,8 +0,0 @@
-import { Result } from "/kinds/Result.ts";
-
-export interface IFakeJsonRepo<T> {
-  queryFirst(): Promise<Result<T, Error>>;
-  queryAll(): Promise<Result<Iterable<T>, Error>>;
-  deleteFirst(): Promise<Result<T, Error>>;
-  insert(item: T): Promise<Result<T, Error>>;
-}
