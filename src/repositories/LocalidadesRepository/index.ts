9a8b8a8006f6f4c0c129ada53418886944d27e3a
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-05 11:18:34 -0400

    refactor: deleted Localidades/index.ts

--- src/repositories/LocalidadesRepository/index.ts
+++
@@ -1,7 +0,0 @@
-import { safeReadJson } from "@/shared/safeReadJson/index.ts";
-import { createQueryAllLocalidades } from "./createLocalidadeRepository.ts";
-
-export const queryAllLocalidades = createQueryAllLocalidades({
-  path: "./private/incorretos.json",
-  safeReadJson,
-});
