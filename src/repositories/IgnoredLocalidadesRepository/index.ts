2767bc18bea5fa83d2d9be3b568bb869e11604bf
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-05 11:36:06 -0400

    feat: insertIgnoredLocalidade

--- src/repositories/IgnoredLocalidadesRepository/index.ts
+++ src/repositories/IgnoredLocalidadesRepository/index.ts
@@ -1,7 +1,13 @@
 import { safeReadJson } from "@/shared/safeReadJson/index.ts";
-import { createQueryFirstLocalidade } from "../LocalidadesRepository/createLocalidadeRepository.ts";
+import { safeWriteJson } from "@/shared/safeWriteJson/index.ts";
+import {
+  createInsertLocalidade,
+} from "@/repositories/LocalidadesRepository/createLocalidadeRepository.ts";
 
-export const queryFirstIgnoredLocalidade = createQueryFirstLocalidade({
-  path: "./private/incorretos.json",
+const IGNORED_LOCALIDADES_FILE_PATH = "./private/incorretos.json";
+
+export const insertIgnoredLocalidade = createInsertLocalidade({
+  path: IGNORED_LOCALIDADES_FILE_PATH,
   safeReadJson,
+  safeWriteJson,
 });
