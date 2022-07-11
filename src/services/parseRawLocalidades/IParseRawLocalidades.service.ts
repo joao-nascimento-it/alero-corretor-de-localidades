507316cbc9eb8a6d18ab87dc7238794116b75f5c
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-11 19:59:55 -0400

    fix: IParseRawLocalidades.service

--- src/services/parseRawLocalidades/IParseRawLocalidades.service.ts
+++ src/services/parseRawLocalidades/IParseRawLocalidades.service.ts
@@ -1,6 +1,6 @@
 import { Result } from "@/kinds/Result.ts";
 
-export type ParseRawLocalidadesService = <E>(
+export type ParseRawLocalidadesService<E> = (
   source: string,
   dest: string,
 ) => Promise<Result<void, E>>;
