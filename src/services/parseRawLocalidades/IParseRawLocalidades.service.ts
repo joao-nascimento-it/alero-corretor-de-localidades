78285ef52b4092c6b26500fd6d8bb56f569256ff
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-11 18:59:19 -0400

    feat: IParseRawLocalidade generic error

--- src/services/parseRawLocalidades/IParseRawLocalidades.service.ts
+++ src/services/parseRawLocalidades/IParseRawLocalidades.service.ts
@@ -1,6 +1,6 @@
 import { Result } from "@/kinds/Result.ts";
 
-export type ParseRawLocalidadesService = (
+export type ParseRawLocalidadesService = <E>(
   source: string,
   dest: string,
-) => Promise<Result<void, Error>>;
+) => Promise<Result<void, E>>;
