1e842822fafcce3959469567d9c6d08a964a1bed
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-10 21:10:26 -0400

    feat: temp

--- src/providers/DistritosProvider/IDistritosProvider.ts
+++ src/providers/DistritosProvider/IDistritosProvider.ts
@@ -1,3 +1,6 @@
-import { Distritos } from "@/models/Distrito.ts";
+import { Distrito, Distritos } from "@/models/Distrito.ts";
 
 export type QueryAllDistritos = () => Promise<Distritos>;
+export type QuerySimilarDistritosByName = (
+  name: Distrito["municipio-nome"],
+) => Promise<Distritos>;
