bad8ce8f32120be216e4eece0bac97b6ab0686b4
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-13 11:27:02 -0400

    test: createQuerySimilarDistritosByName

--- src/providers/DistritosProvider/DistritosProvider.test.ts
+++ src/providers/DistritosProvider/DistritosProvider.test.ts
@@ -1,5 +1,8 @@
 import { assertEquals } from "@/deps.ts";
-import { createQueryAllDistritos } from "./createDistritosProvider.ts";
+import {
+  createQueryAllDistritos,
+  createQuerySimilarDistritosByName,
+} from "./createDistritosProvider.ts";
 
 Deno.test("Distrito Provider", async () => {
   const queryAllDistritos = createQueryAllDistritos([{
@@ -20,5 +23,40 @@ Deno.test("Distrito Provider", async () => {
     "municipio-nome": "Porto Velho",
     "UF-id": "11",
     "UF-sigla": "RO",
+  }]);
+});
+
+Deno.test("createQuerySimilarDistritosByName", async () => {
+  const querySimilarDistritosByName = createQuerySimilarDistritosByName([{
+    "distrito-id": "120040105",
+    "distrito-nome": "Rio Branco",
+    "municipio-id": "1200401",
+    "municipio-nome": "Rio Branco",
+    "UF-id": "12",
+    "UF-sigla": "AC",
+  }, {
+    "distrito-id": "110020505",
+    "distrito-nome": "Porto Velho",
+    "municipio-id": "1100205",
+    "municipio-nome": "Porto Velho",
+    "UF-id": "11",
+    "UF-sigla": "RO",
+  }]);
+
+  const distritos = await querySimilarDistritosByName("Porto Velho");
+  assertEquals(distritos, [{
+    "distrito-id": "110020505",
+    "distrito-nome": "Porto Velho",
+    "municipio-id": "1100205",
+    "municipio-nome": "Porto Velho",
+    "UF-id": "11",
+    "UF-sigla": "RO",
+  }, {
+    "distrito-id": "120040105",
+    "distrito-nome": "Rio Branco",
+    "municipio-id": "1200401",
+    "municipio-nome": "Rio Branco",
+    "UF-id": "12",
+    "UF-sigla": "AC",
   }]);
 });
