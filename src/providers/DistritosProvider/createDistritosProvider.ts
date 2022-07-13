ce066dd8ee2c4ae54d721823054d46bbd315da61
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-07-13 11:26:48 -0400

    feat: createQuerySimilarDistritosByName

--- src/providers/DistritosProvider/createDistritosProvider.ts
+++ src/providers/DistritosProvider/createDistritosProvider.ts
@@ -1,10 +1,26 @@
 import { Distritos } from "@/models/Distrito.ts";
-import { QueryAllDistritos } from "./IDistritosProvider.ts";
+import { sortBySimilarString } from "@/shared/sortBySimilarString/index.ts";
+import {
+  QueryAllDistritos,
+  QuerySimilarDistritosByName,
+} from "./IDistritosProvider.ts";
 
 export function createQueryAllDistritos(
   distritos: Distritos,
 ): QueryAllDistritos {
   return async () => {
     return await distritos;
+  };
+}
+
+export function createQuerySimilarDistritosByName(
+  distritos: Distritos,
+): QuerySimilarDistritosByName {
+  return async (name) => {
+    return await sortBySimilarString(
+      name,
+      distritos,
+      (item) => item["distrito-nome"],
+    );
   };
 }
