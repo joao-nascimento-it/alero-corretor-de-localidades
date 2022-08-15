b6c1d6ccab5125331bb7f97c0f5d9e4e37132261
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-08-15 18:20:44 -0400

    build: created task test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,3 @@
-console.log("Please execute 'denon parseRaw' script");
+console.log(
+  "Please execute 'deno task parse-raw'\nor 'deno task update-localidade'\nor 'deno task create-database-script' script",
+);
